66481ce4e6f387bcb73e262bfbbc1826e368e3a5
Pedro <pedromsouza97@gmail.com>
2025-03-23 19:05:21 -0300

    Atualizacao no index do Groups e adc Button e ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GrupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
 
 import { Container } from "./styles";
 
 export default function Groups() {
+    const [groups, setGroups] = useState([]);
+
     return (
         <Container>
             <Header />
             <Highlight 
             title="Turmas" 
             subtitle="jogue com a sua turma" />
-
-            <GrupCard title="Galera do Ignite" />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                <GrupCard 
+                    title={item} 
+                />
+                )}
+                contentContainerStyle={groups.length === 0 && {flex: 1}}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="Que tal cadastrar a primeira turma?" 
+                    />
+                )} 
+            />
         </Container>
     );
 }
